b9436fb649501a9ffc589224c02da4d93ed22a12
Andrew <andrewzamai@hotmail.com>
2023-12-15 19:20:25 +0100

    added music ne guidelines and new music MSEQA dataset with guidelines

--- data_handlers/data_handler_cross_NER.py
+++ data_handlers/data_handler_cross_NER.py
@@ -6,11 +6,15 @@ Contains 1 general domain NER dataset CoNLL2003, and 5 domain specific datasets:
 - politics, science, literature, music, ai
 Each domain specific dataset has its own defined set of NE categories.
 """
-
+import json
 # importing packages
 import os
+import random
+import re
+
 from datasets import Dataset, DatasetDict
 
+from data_handlers.data_handler_pileNER import has_more_than_n_foreign_chars, has_too_many_newline, has_too_many_whitespaces, has_too_many_punctuations_and_digits, split_into_sentences, count_target_words
 
 # read sentences with BIO labelling from txt file
 def read_bio_file(path_to_bio_txt):
@@ -165,7 +169,7 @@ def load_questions_from_txt(path_to_txt):
 
 
 # build DatasetDict object with docContext-question-goldAnswers
-def build_dataset_QA_format(dataset_dict, path_to_questions_txt):
+def build_dataset_MSEQA_format(dataset_dict, path_to_questions_txt):
     # loading questions associated to each NE category
     questions = load_questions_from_txt(path_to_questions_txt)
     print(questions)
@@ -201,14 +205,146 @@ def build_dataset_QA_format(dataset_dict, path_to_questions_txt):
             newDataset_Dataset[splitName] = Dataset.from_list(newDataset_dict[splitName])
 
     new_dataset_dict = DatasetDict(newDataset_Dataset)
-    new_dataset_dict["dataset_name"] = dataset_dict["dataset_name"]
+    # new_dataset_dict["dataset_name"] = dataset_dict["dataset_name"]
+
+    return new_dataset_dict
+
+def get_one_sentence_from_sample(ne_type, document_context, ne_occurrences):
+    # split in sentences according to punctuation .?!
+    sentences = split_into_sentences(document_context)
+    target_words = [ne[0] for ne in ne_occurrences]
+    # print(target_words)
+    # count the occurrences of target words in each sentence
+    # to return the one with at least 1/highest number of occ.
+    target_word_counts = []
+    for sentence in sentences:
+        occurrences_count, target_words_found = count_target_words(sentence, target_words)
+        target_word_counts.append({"sentence": sentence,
+                                   "target_words_in_it": target_words_found,
+                                   "occurrences_count": occurrences_count
+                                   })
+
+    # sort sentences by decreasing occurrences_count
+    target_word_counts = sorted(target_word_counts, key=lambda x: x['occurrences_count'], reverse=True)
+    # returning the sentence with highest number of occurrences, but with some contraints
+    sentence_to_ret = None
+    i = 0
+    while i < len(target_word_counts):
+        if target_word_counts[i]['occurrences_count'] != 0:
+            if 50 < len(target_word_counts[i]['sentence']) < 200:
+                if not has_too_many_whitespaces(target_word_counts[i]['sentence'], 4):
+                    if not has_too_many_newline(target_word_counts[i]['sentence'], 1):
+                        if not has_more_than_n_foreign_chars(target_word_counts[i]['sentence'], 2):
+                            if not has_too_many_punctuations_and_digits(target_word_counts[i]['sentence'], 10):
+                                sentence_to_ret = target_word_counts[i]
+                                break
+            elif ne_type in ['misc', 'musicalinstrument', 'person']:
+                sentence_to_ret = target_word_counts[i]
+                break
+        i += 1
+
+    return sentence_to_ret
+
+def get_n_sentences_per_ne_type(dataset_BIO_format, ne_types_list, n_sentences_per_ne=3):
+    # getting from training set n_sentences_per_ne as positive examples from which to let gpt infer NE definition
+    sentences_per_ne_type = {ne: [] for ne in ne_types_list}
+    trainDataset = dataset_BIO_format['train'].to_list()
+    random.seed(4)
+    random.shuffle(trainDataset)
+    for ne_type in ne_types_list:
+        i = 0
+        while len(sentences_per_ne_type[ne_type]) < n_sentences_per_ne and i < len(trainDataset):
+            sample = trainDataset[i]
+            doc_ID, tokens, labels = sample.values()
+            doc_metadata = get_doc_metadata_with_start_end_char_indexes(dataset_BIO_format, tokens, labels)
+            if doc_metadata[ne_type]:
+                occurrences_for_this_ne = doc_metadata[ne_type]
+                document_context = ' '.join([str(elem) for elem in tokens])
+                sentence_target_words = get_one_sentence_from_sample(ne_type, document_context, occurrences_for_this_ne)
+                if sentence_target_words is not None:
+                    # removing duplicates in list of target words
+                    sentence_target_words['target_words_in_it'] = list(set(sentence_target_words['target_words_in_it']))
+                    sentences_per_ne_type[ne_type].append(sentence_target_words)
+            i += 1
+
+    not_enough_sentences = []
+    for ne_type, sentences in sentences_per_ne_type.items():
+        if len(sentences) < n_sentences_per_ne:
+            # raise ValueError(f"not enough sentences for {ne_type}")
+            not_enough_sentences.append((ne_type, len(sentences)))
+    print(f"NE types with less than n_sentences_per_ne: {len(not_enough_sentences)}")
+    print(not_enough_sentences)
+
+    return sentences_per_ne_type
+
+
+def build_dataset_MSEQA_format_with_guidelines(path_to_crosNER_datasets, subdataset_name, path_to_ne_definitions_json):
+    # datasetDict in BIO format
+    dataset_BIO_format = build_dataset_from_txt(os.path.join(path_to_crosNER_datasets, subdataset_name))
+    # ne_types_list
+    ne_types_list = get_ne_categories_only(dataset_BIO_format)
+    # definitions for each ne
+    with open(path_to_ne_definitions_json, 'r') as file:
+        subdataset_NEs_guidelines = json.load(file)
+
+    newDataset_dict = {splitName: [] for splitName in dataset_BIO_format.keys() if splitName != "dataset_name"}
+    newDataset_Dataset = {splitName: None for splitName in dataset_BIO_format.keys() if splitName != "dataset_name"}
+    for splitName in dataset_BIO_format.keys():
+        if splitName != "dataset_name":
+            for i in range(len(dataset_BIO_format[splitName])):
+                docID, tokens, labels = get_document_id_tokens_labels(dataset_BIO_format, splitName, i)
+                docMetadata = get_doc_metadata_with_start_end_char_indexes(dataset_BIO_format, tokens, labels)
+                question_number = 0
+                for tagName in ne_types_list:
+                    # question
+                    gpt_definition = subdataset_NEs_guidelines[tagName]['gpt_answer'].strip()
+                    # print(gpt_definition.strip())
+                    # gpt answer may have been truncated, ensure it ends by "} before evaluating to dict
+                    if not gpt_definition.endswith("}"):
+                        if not gpt_definition.endswith("\""):
+                            gpt_definition += "\""
+                        gpt_definition += "}"
+                    # print(gpt_definition)
+                    this_ne_guidelines = eval(gpt_definition)
+                    # replacing ne types occurrences between single quotes to their UPPERCASE
+                    tagName_in_guidelines = subdataset_NEs_guidelines[tagName]['named_entity']
+                    pattern = re.compile(rf'\'{re.escape(tagName_in_guidelines)}\'')
+                    this_ne_guidelines = {k: pattern.sub(f'{tagName_in_guidelines.upper()}', v) for k, v in this_ne_guidelines.items()}
+
+                    question = f"Your task is to extract the Named Entities of type {tagName_in_guidelines.upper()} from an input TEXT. "
+                    question += "You are given a DEFINITION and some GUIDELINES.\n"
+                    question += "DEFINITION: " + this_ne_guidelines['Definition'] + "\nGUIDELINES: " + this_ne_guidelines['Guidelines'] + "\n"
+                    question += f"TEXT: "
+
+                    # splitName:docID:questioNumberForThatDocument
+                    doc_question_pairID = docID + ':' + str(question_number)
+                    question_number += 1
+                    # document context
+                    context = ' '.join([str(elem) for elem in tokens])
+                    # retrieving gold answers for this tagName
+                    goldAnswers = docMetadata[tagName]
+                    answers = {'answer_start': [], 'text': []}
+                    for ga in goldAnswers:
+                        answers['answer_start'].append(ga[1])
+                        answers['text'].append(ga[0])
+                    sample = {'doc_question_pairID': doc_question_pairID,
+                              'document_context': context,
+                              'tagName': tagName,
+                              'question': question,
+                              'answers': answers
+                              }
+                    newDataset_dict[splitName].append(sample)
+            newDataset_Dataset[splitName] = Dataset.from_list(newDataset_dict[splitName])
+
+    new_dataset_dict = DatasetDict(newDataset_Dataset)
+    # new_dataset_dict["dataset_name"] = dataset_dict["dataset_name"]
 
     return new_dataset_dict
 
 
 if __name__ == '__main__':
 
-    path_to_cross_NER_datasets = "../../datasets/CrossNER/ner_data"
+    path_to_cross_NER_datasets = "../../../datasets/CrossNER/ner_data"
     splits = ["train", "validation", "test"]
 
     # sentences = read_bio_file(os.path.join(path_to_cross_NER_datasets, 'music', 'dev.txt'))
@@ -220,15 +356,16 @@ if __name__ == '__main__':
     print("Dataset features: ")
     print(music_dataset["train"].features)
 
-    print("dataset_name")
-    print(music_dataset["dataset_name"])
+    #print("dataset_name")
+    #print(music_dataset["dataset_name"])
 
     print("\nNE categories: ")
     ne_categories_music = get_ne_categories_labels_unique(music_dataset)
     print(len(ne_categories_music))
     print(ne_categories_music)
 
-    print(get_ne_categories_only(music_dataset))
+    ne_types_list = get_ne_categories_only(music_dataset)
+    print(ne_types_list)
 
     print("\nSplits statistics (number of occurrences per NE category): ")
     ne_categories_statistics = get_ne_categories_statistics(music_dataset)
@@ -244,6 +381,18 @@ if __name__ == '__main__':
     # converting BIO labeling to QA metedata
     print(get_doc_metadata_with_start_end_char_indexes(music_dataset, tokens, labels))
 
+    #sentences_per_ne_type = get_n_sentences_per_ne_type(music_dataset, ne_types_list, n_sentences_per_ne=3)
+    #print(sentences_per_ne_type)
+    #with open("./questions/crossNER/sentences_per_ne_type_music.json", 'w') as f:
+        #json.dump(sentences_per_ne_type, f, indent=2)
+
+    music_dataset_MSEQA_format_with_guidelines = build_dataset_MSEQA_format_with_guidelines(path_to_cross_NER_datasets, 'music', "./questions/crossNER/music_NE_definitions.json")
+    print(music_dataset_MSEQA_format_with_guidelines)
+    print(music_dataset_MSEQA_format_with_guidelines['train'][0])
+    print(music_dataset_MSEQA_format_with_guidelines['train'][1])
+    print(music_dataset_MSEQA_format_with_guidelines['train'][23])
+
+    """
     path_to_questions = os.path.join("./cross_ner_questions/", dataset_name + ".txt")
     questions = load_questions_from_txt(path_to_questions)
     print(questions)
@@ -256,3 +405,4 @@ if __name__ == '__main__':
     print(dataset_QA_format["train"][0])
     print(dataset_QA_format["train"][1])
     print(dataset_QA_format["train"][23])
+    """
--- evaluate_on_test.py
+++ evaluate_on_test.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
 
     """ -------------------- Evaluation parameters -------------------- """
 
+    """
     from data_handlers import data_handler_pileNER as data_handler_MSEQA_dataset
 
     path_to_dataset_MSEQA_format = './datasets/pileNER/min_occur_100_MSEQA'
@@ -33,13 +34,33 @@ if __name__ == '__main__':
     EVAL_BATCH_SIZE = 128
 
     MAX_ANS_LENGTH_IN_TOKENS = 10
+    """
+
+    from data_handlers import data_handler_cross_NER as data_handler_MSEQA_dataset
+
+    subdataset_name = 'music'
+    path_to_cross_NER_datasets = './datasets/crossNER/BIO_format'
+    path_to_subdataset_guidelines = f"./MSEQA_4_NER/data_handlers/questions/crossNER/{subdataset_name}_NE_definitions.json"
+
+    path_to_dataset_MSEQA_format = './datasets/crossNER/MSEQA_format_guidelines/music'
+    tokenizer_to_use = "roberta-base"
+    path_to_model = "./finetunedModels/MSEQA_pileNER_prefix_pretrained_bb_CP"
+
+    MAX_SEQ_LENGTH = 512  # question + context + special tokens
+    DOC_STRIDE = 50  # overlap between 2 consecutive passages from same document
+    MAX_QUERY_LENGTH = 150  # not used, but questions must not be too long given a chosen DOC_STRIDE
+
+    EVAL_BATCH_SIZE = 64
+
+    MAX_ANS_LENGTH_IN_TOKENS = 10
 
     print(f"Evaluating MS-EQA model named {path_to_model.split('/')[-1]} on {path_to_dataset_MSEQA_format.split('/')[-1]} test set\n")
 
     print("Loading train/validation/test Datasets in MS-EQA format...")
     if not os.path.exists(path_to_dataset_MSEQA_format):
         print(" ...building Datasets from huggingface repository in MS-EQA format")
-        dataset_MSEQA_format = data_handler_MSEQA_dataset.build_dataset_MSEQA_format()
+        # dataset_MSEQA_format = data_handler_MSEQA_dataset.build_dataset_MSEQA_format()
+        dataset_MSEQA_format = data_handler_MSEQA_dataset.build_dataset_MSEQA_format_with_guidelines(path_to_cross_NER_datasets, subdataset_name, path_to_subdataset_guidelines)
         # removing outliers
         # dataset_MSEQA_format = data_handler_MSEQA_dataset.remove_outlier_ne_types(dataset_MSEQA_format, 100)
         dataset_MSEQA_format.save_to_disk(path_to_dataset_MSEQA_format)
